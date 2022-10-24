2eca06842530b0dd0fd54881be83d5b5e95f350e
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2022-10-24 21:52:19 +0500

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PPBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    public TestController(){}
+
+    [HttpGet(Name = "GetTest")]
+    public void GetTest()
+    {
+        
+    }
+}
