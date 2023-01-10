473f2ecd2a555684b16df939bf1acc2cf67432fb
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-01-10 22:36:49 +0500

    add name to group model

--- Models/Group.cs
+++ Models/Group.cs
@@ -7,12 +7,16 @@ public class Group : IDatabaseModel
     public Group(int id, GroupRegistrationModel grm)
     {
         Id = id;
+        Name = grm.Name;
         Members = grm.Members;
         Tests = grm.Tests;
     }
 
     [BsonId]
     public int Id { get; set; }
+    
+    [BsonElement("name")]
+    public string Name { get; set; } = null!;
     
     [BsonElement("members")]
     public List<int> Members { get; set; } = new();
@@ -30,6 +34,7 @@ public class Group : IDatabaseModel
 
 public class GroupRegistrationModel
 {
+    public string Name { get; set; } = null!;
     public List<int> Members { get; set; } = new();
     
     public List<int> Tests { get; set; } = new();
